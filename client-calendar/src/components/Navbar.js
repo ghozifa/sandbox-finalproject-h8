2cb70d894c11226f86b33a746272aa0d0d6e245b
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-09-09 01:21:54 +0700

    fix navbar

--- client-calendar/src/components/Navbar.js
+++ client-calendar/src/components/Navbar.js
@@ -6,8 +6,8 @@ function Navbar() {
   const handleLogout = () => {
     localStorage.clear();
     navigate("/login");
-    Swal.fire("Logout Success")
-  }
+    Swal.fire("Logout Success");
+  };
 
   return (
     <div className="Navbar">
@@ -62,14 +62,16 @@ function Navbar() {
                   Register
                 </Link>
               </li>
-              <li>
-                <button
-                  onClick={handleLogout}
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                 Logout
-                </button>
-              </li>
+              {localStorage.getItem("access_token") ? (
+                <li>
+                  <button
+                    onClick={handleLogout}
+                    className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  >
+                    Logout
+                  </button>
+                </li>
+              ) : null}
             </ul>
           </div>
         </div>
